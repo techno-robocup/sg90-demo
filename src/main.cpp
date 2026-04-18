b33a1875a0ebb1bbe39487e55873754de39656d4
rotarymars <s.goto2050@gmail.com>
2026-04-18 11:21:43 +0900

    fixing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-constexpr uint8_t SG90_PIN = 9;
+constexpr uint8_t SG90_PIN = 13;
 constexpr int STEP_DELAY_MS = 15;
+constexpr int SERVO_MIN_US = 500;
+constexpr int SERVO_MAX_US = 2400;
+constexpr int SERVO_FREQ_HZ = 50;
 
 Servo sg90;
 
 void setup() {
-  sg90.attach(SG90_PIN);
+  ESP32PWM::allocateTimer(0);
+  sg90.setPeriodHertz(SERVO_FREQ_HZ);
+  sg90.attach(SG90_PIN, SERVO_MIN_US, SERVO_MAX_US);
 }
 
 void loop() {
